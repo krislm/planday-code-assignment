f97447739ec16abd4df366764f319c80409b3fff
krislm <mcabe@me.com>
2020-08-22 23:10:50 +0200

    prod build

---
+++ build/precache-manifest.ba729093ca03b94ea9e6061c1555400c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edaf14671b280acdf546735844b944cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c21abf48e7185313c05b",
+    "url": "/static/css/main.05b5e8f2.chunk.css"
+  },
+  {
+    "revision": "6acb0e77b8f799ed3eb9",
+    "url": "/static/js/2.a3ad5844.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a3ad5844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21abf48e7185313c05b",
+    "url": "/static/js/main.c53275d2.chunk.js"
+  },
+  {
+    "revision": "dcf09a4a23e30af825f7",
+    "url": "/static/js/runtime-main.18c98b5e.js"
+  }
+]);
